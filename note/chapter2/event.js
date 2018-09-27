b5cae844264796426dac55d54817a1d7a5b69500
XXB <xxblcw@163.com>
2018-09-27 23:47:25 +0800

    event的异步机制实现

--- note/chapter2/event.js
+++ note/chapter2/event.js
@@ -7,5 +7,61 @@ class Evente {
             this.map[name].push(fn);
             return;
         }
+        this.map[name] = [fn];
+        return;
+    }
+    emite(name, ...args) {
+        this.map[name].forEach(fn => {
+            fn(...args);
+        });
     }
 }
+
+let e = new Evente();
+
+e.add("hello", (err, name) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(name);
+});
+
+e.emite("hello", "Evente发生了错误");
+e.emite("hello", null, "hello Evente");
+
+
+/***************** 支持链式调用 ***********************/
+
+
+class ChainEvente {
+    constructor() {
+        this.map = {};
+    }
+    add(name, fn) {
+        if (this.map[name]) {
+            this.map[name].push(fn);
+            return;
+        }
+        this.map[name] = [fn];
+        return this;
+    }
+    emite(name, ...args) {
+        this.map[name].forEach(fn => {
+            fn(...args);
+        });
+        return this;
+    }
+}
+
+let e2 = new ChainEvente();
+
+e2.add("hello", (err, name) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(name);
+})
+.emite("hello", "ChainEvente发生了错误")
+.emite("hello", null, "hello ChainEvente");
