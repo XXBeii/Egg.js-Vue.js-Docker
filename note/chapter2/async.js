a753953cfefed98d3335bbbb8f2a8dc7d0f5a3db
XXB <xxblcw@163.com>
2018-10-01 16:39:33 +0800

    async异步

---
+++ note/chapter2/async.js
@@ -0,0 +1,57 @@
+async function func() {
+    return 2;
+}
+
+func().then(console.log);
+
+const getPosts = () =>
+    new Promise((resolve, reject) => {
+        resolve([
+            {
+                name: "a"
+            },
+            {
+                name: "b"
+            },
+            {
+                name: "c"
+            },
+            {
+                name: "d"
+            }
+        ]);
+    });
+
+async function func2() {
+    try {
+        const number = await func();
+        const posts = await getPosts();
+        console.log('func2 number', number);
+        console.log('func2 posts', posts);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+func2();
+
+const fs = require("fs");
+
+const readFilePromise = filename =>
+    new Promise((resolve, reject) => {
+        fs.readFile(filename, (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        });
+    });
+
+async function main() {
+    const txt = await readFilePromise("mock.txt");
+    console.log(txt.toString());
+    return 'main Promise'
+}
+
+main().then(console.log);
