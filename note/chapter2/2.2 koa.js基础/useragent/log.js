9a898a7ca5e5c1d647af77017668cc54b157a43e
XXB <xxblcw@163.com>
2018-10-10 23:48:42 +0800

    2.2备注

--- note/chapter2/2.2 koa.js基础/useragent/log.js
+++ note/chapter2/2.2 koa.js基础/useragent/log.js
@@ -2,7 +2,11 @@ module.exports = options => {
     if (!options.format) {
         console.error("需要传递 format 函数");
     }
+    // 函数为外部自定义的format
+    // ctx参数来自上个中间件
+    // 中间件完美衔接二者并实现异步处理
     return async (ctx, next) => {
+        // ctx来自上个中间件传递的参数
         console.log(options.format(JSON.stringify(ctx)));
         await next()
     };
